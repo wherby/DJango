d591bbe5339dc758763ef1df9b3f6bcc9604b543
wherby <187225577@qq.com>
2014-05-12 00:26:11 +0800

    change URL

--- cms/cms/urls.py
+++ cms/cms/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url(r'^tinymce/(?P<path>.*)$','django.views.static.serve',
         {'document_root':os.path.normcase('C:/Python27\\Lib\\site-packages\\django\\bin\\cms\\tinymce\\js\\tinymce')}),
     url(r'^search', 'search.views.search'),
-    url(r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$,','coltrane.views.entry_detail'),
+    url(r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$','coltrane.views.entry_detail'),
     url(r'^weblog/$', 'coltrane.views.entries_index'),
     url(r'',include('django.contrib.flatpages.urls')),
 )
